6b4bd0ea20464d908baaacdd7399f1f7d2457809
Craig <tingeycraig@gmail.com>
2016-03-15 21:29:47 -0600

    d

--- dna_alignment/jobs/queuejobs.py
+++ dna_alignment/jobs/queuejobs.py
@@ -20,7 +20,8 @@ for t in tuples[:5]:
     pair.write("from compare import compare\ncompare('/diversity/%s','/diversity/%s','%s')" % (t[0],t[1], t[0][:4]+'_'+t[1][:4]))
     pair.close()
     time.sleep(1)
-    # os.system('sbatch compare.sh')
+    os.system('cat ../pair.py')
+    os.system('sbatch compare.sh')
 
 # human vs apes
 # ape_list = os.listdir('/home/A01055143/AdvancedAlgorithms/dna_alignment/great_apes')
