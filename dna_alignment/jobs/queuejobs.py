21b579180dd88e9d3fda6af893a3e2ca1d492497
Craig <tingeycraig@gmail.com>
2016-03-15 21:14:58 -0600

    t

--- dna_alignment/jobs/queuejobs.py
+++ dna_alignment/jobs/queuejobs.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 # human vs neandertal
 # pair = open('/home/A01055143/AdvancedAlgorithms/dna_alignment/pair.py', 'w')
@@ -13,14 +14,12 @@ tuples = [(x,y) for x in L1 for y in L1 if x != y]
 for entry in tuples:
     if (entry[1], entry[0]) in tuples:
         tuples.remove((entry[1],entry[0]))
-for t in tuples:
+for t in tuples[:5]:
     pair = open('/home/A01055143/AdvancedAlgorithms/dna_alignment/pair.py', 'w')
     pair.write("from compare import compare\ncompare('/diversity/%s','/diversity/%s','%s')" % (t[0],t[1], t[0][:4]+'_'+t[1][:4]))
     pair.close()
-    pair = open('/home/A01055143/AdvancedAlgorithms/dna_alignment/pair.py')
-    print pair.readlines()
-    pair.close()
-    # os.system('sbatch compare.sh')
+    time.sleep(1)
+    os.system('sbatch compare.sh')
 
 # human vs apes
 # ape_list = os.listdir('/home/A01055143/AdvancedAlgorithms/dna_alignment/great_apes')
