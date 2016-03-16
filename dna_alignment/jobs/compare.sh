cb3092fc3063fe5380b06a83eec2c3c8d7f6f8a5
Craig <tingeycraig@gmail.com>
2016-03-15 20:17:27 -0600

    fix

--- dna_alignment/jobs/compare.sh
+++ dna_alignment/jobs/compare.sh
@@ -4,4 +4,4 @@
 #SBATCH --time 2-00:00:00
 #SBATCH --job-name dna_first_go
 
-python /home/A01055143/dna_project/dna_alignment/pair.py
+python /home/A01055143/AdvancedAlgorithms/dna_alignment/pair.py
