e9210fb94ad0d6bba410aaa50781e4d4e584fa92
Craig <tingeycraig@gmail.com>
2016-03-15 22:52:17 -0600

    cleaning up

--- dna_alignment/compare.py
+++ dna_alignment/compare.py
@@ -1,6 +1,5 @@
 #!/bin/sh
 from alignment import Hirschberg
-import thread
 
 def compare(filea, fileb, outputfile):
     human = open('/home/A01055143/AdvancedAlgorithms/dna_alignment'+filea)
@@ -16,5 +15,4 @@ def compare(filea, fileb, outputfile):
 
     a,b = hirsch.align(hum, nea)
     final = hirsch.score(a,b, '/home/A01055143/AdvancedAlgorithms/dna_alignment/results/'+outputfile)
-    # print 'final score: ', final
     print 'finished %s and %s' % (filea, fileb)
--- dna_alignment/main.py
+++
@@ -1,5 +0,0 @@
-from dna_align import dna_align
-
-dna = dna_align()
-
-print dna.recursive('AAAA', 'CCCC')
