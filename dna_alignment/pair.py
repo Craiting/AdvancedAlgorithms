6b9cd09d6f5dc80207eae70d0b756371e68232ef
Craig Tingey <A01055143@login-01.rc.usu.edu>
2016-03-15 20:27:15 -0600

    c

--- dna_alignment/pair.py
+++ dna_alignment/pair.py
@@ -1,2 +1,2 @@
 from compare import compare
-compare('/diversity/chinese.txt','/diversity/papuanuguini.txt','chin_papu')
+compare('/human_neandertal/homosapian.txt','/human_neandertal/Neandertal.txt','human_neandertal')
