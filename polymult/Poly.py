bf38eaf9166eb9011a6acc67380c8fac8924c2a6
Craig <tingeycraig@gmail.com>
2016-03-22 20:13:37 -0600

    started the divide and conquer algorithm.

--- polymult/Poly.py
+++ polymult/Poly.py
@@ -9,3 +9,16 @@ class PolyMult:
                 out[i+j] = out[i+j] + a[i] * b[j]
         return out        
 
+    def divide_conquer(self, p, q):
+        if len(p) == len(q) == 1:
+            return [p[0]*q[0]]
+
+        mid = len(p)/2
+        pl = p[:mid]
+        ph = p[mid:]
+        ql = q[:mid]
+        qh = q[mid:]
+        sol1 = divide_conquer(pl,ql)
+        sol2 = divide_conquer(ph,qh)
+        sol3 = divide_conquer(sum(pl,ph),sum(ql,qh))
+        midsol = sol3 - sol1 - sol2
