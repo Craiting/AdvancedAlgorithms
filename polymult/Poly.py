ee1dcc707bfa0abaa3e9f363ddb37f70189a992e
Craig <tingeycraig@gmail.com>
2016-03-22 23:25:29 -0600

    added poly operations

--- polymult/Poly.py
+++ polymult/Poly.py
@@ -1,13 +1,13 @@
 
 
 class PolyMult:
-    
+
     def simple(self,a, b):
         out = [0 for x in range(len(a)+len(b)-1)]
         for i in range(len(a)):
             for j in range(len(b)):
                 out[i+j] = out[i+j] + a[i] * b[j]
-        return out        
+        return out
 
     def divide_conquer(self, p, q):
         if len(p) == len(q) == 1:
@@ -18,7 +18,28 @@ class PolyMult:
         ph = p[mid:]
         ql = q[:mid]
         qh = q[mid:]
+        sol3 = divide_conquer(sum(pl,ph),sum(ql,qh)) # make add polynomial function
         sol1 = divide_conquer(pl,ql)
         sol2 = divide_conquer(ph,qh)
-        sol3 = divide_conquer(sum(pl,ph),sum(ql,qh))
-        midsol = sol3 - sol1 - sol2
+        midsol = sol3 - sol1 - sol2 # make subtract polynomial function
+        return sol1 + midsol + sol2
+
+    def add_poly(self, a, b):
+        if len(a) <= len(b):
+            for i in range(len(a)):
+                b[i] = b[i] + a[i]
+            return b
+        else:
+            for i in range(len(b)):
+                a[i] = a[i] + b[i]
+            return a
+
+    def sub_poly(self, a, b):
+        if len(a) <= len(b):
+            for i in range(len(a)):
+                b[i] = a[i] - b[i]
+            return b
+        else:
+            for i in range(len(b)):
+                a[i] = a[i] - b[i]
+            return a
