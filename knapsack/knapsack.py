4e06494801642ef4e29b24cb8f904381cd944709
Craig <tingeycraig@gmail.com>
2016-04-27 09:21:52 -0600

    fixed" \

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -2,18 +2,19 @@ import random
 import time
 import numpy
 MILLI = 1000
-
-cache = {}
-def knap(objs, sac):
-    if (len(objs),sac) in cache and len(objs) != 22:
+def knapsack(o,s):
+    cache = {}
+    def knap(objs, sac):
+        if (len(objs),sac) in cache and len(objs) != 22:
+            return cache[(len(objs),sac)]
+        if sac < 0:
+            return 99999999999999999999l
+        if len(objs) == 0:
+            return sac
+        cache[(len(objs),sac)] = min(knap(objs[1:], sac-objs[0]), # Do use
+                                     knap(objs[1:], sac))       # Dont use
         return cache[(len(objs),sac)]
-    if sac < 0:
-        return 99999999999999999999l
-    if len(objs) == 0:
-        return sac
-    cache[(len(objs),sac)] = min(knap(objs[1:], sac-objs[0]), # Do use
-                                 knap(objs[1:], sac))       # Dont use
-    return cache[(len(objs),sac)]
+    return knap(o,s)
 
 def create_point(m):
     print m
@@ -21,24 +22,25 @@ def create_point(m):
     k = 11.0000
     objects = [int(float(str(random.uniform(0,1.0))[:m])*10**m) for x in range(n)]
     start = time.clock()
-    sol = knap(objects, k*10**m)
+    sol = knapsack(objects, k*10**m)
     stop = time.clock()
     totaltime_ms = (stop - start) * MILLI
     return (totaltime_ms, float(sol)/(10**m))
 
+print 'it begins'
 points = {12:[],11:[],10:[],9:[],8:[],7:[],6:[],5:[],4:[],3:[]}
 # p = create_point(12)
 # print p
 for j in range(30):
-   for i in range(3,13)[::-1]:
-       p = create_point(i)
-       points[i].append(p)
-   print j
+    for i in range(3,13)[::-1]:
+        p = create_point(i)
+        points[i].append(p)
+    # print j
 
-f = open('results3.txt', 'w')
+f = open('res.txt', 'w')
 real_answer = float(numpy.mean([x[1] for x in points[12]]))
 for key in points:
-    f = open('results3.txt','a')
+    f = open('res.txt','a')
     f.write('m = %d\n' % key)
     for point in points[key]:
         f.write('(%f, %f)\n' % (point[0],abs(real_answer - point[1])))
