338873a8501ca923c02a1f8d319340d9c16c6351
Craig Tingey <A01055143@login-03.rc.usu.edu>
2016-04-29 08:47:36 -0600

    more stuff

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -1,6 +1,7 @@
 import random
 import time
 import numpy
+import math
 MILLI = 1000
 def knapsack(o,s):
     cache = {}
@@ -20,7 +21,7 @@ def create_point(m):
     print m
     n = 22
     k = 11.0000
-    objects = [int(float(str(random.uniform(0,1.0))[:m])*10**m) for x in range(n)]
+    objects = [int(math.floor(float(round(random.uniform(0,1.0),m))*10**m)) for x in range(n)]
     start = time.clock()
     sol = knapsack(objects, k*10**m)
     sol = knapsack(objects, k*10**m)
@@ -34,19 +35,21 @@ def create_point(m):
     return (totaltime_ms, float(sol)/(10**m))
 
 print 'it begins'
-points = {12:[],11:[],10:[],9:[],8:[],7:[],6:[],5:[],4:[],3:[]}
+points = {11:[],10:[],9:[],8:[],7:[],6:[],5:[],4:[],3:[]}
 # p = create_point(12)
 # print p
 for j in range(30):
-    for i in range(3,13)[::-1]:
+    for i in range(3,12)[::-1]:
         p = create_point(i)
         points[i].append(p)
     # print j
 
-f = open('final_results.txt', 'w')
-real_answer = float(numpy.mean([x[1] for x in points[12]]))
+f = open('work.txt', 'w')
+#real_answer = float(numpy.mean([x[1] for x in points[12]]))
+r = create_point(12)
+real_answer = r[1]
 for key in points:
-    f = open('final_results.txt','a')
+    f = open('work.txt','a')
     f.write('m = %d\n' % key)
     for point in points[key]:
         f.write('(%f, %f)\n' % (point[0],abs(real_answer - point[1])))
